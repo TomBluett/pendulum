7e42320c0ad8c95ee8486597dffb319deb3ad0d5
Tom Bluett <tbluett@me.com>
2014-12-14 15:54:21 -0500

    animate squares

--- js/pendulum.js
+++ js/pendulum.js
@@ -1,3 +1,5 @@
+var interval;
+
 function canvas () {
   var context = myCanvas.getContext('2d');
   var x = 0, y = 0, width = myCanvas.width, height = myCanvas.height;
@@ -11,12 +13,19 @@ function canvas () {
     var n = 10;
     width = 20;
     height = 20;
-    while (i < n) {
-      x = i/10 * myCanvas.width;
-      y = i/10 * myCanvas.height;
-      context.fillRect(x, y, width, height);
-      i++;
+    var time = 0;
+    var redraw = function () {
+      context.clearRect(0, 0, myCanvas.width, myCanvas.height);
+      for (i=0; i < n; i++) {
+      //while (i < n) {
+        x = i/10 * myCanvas.width + time;
+        y = i/10 * myCanvas.height;
+        context.fillRect(x, y, width, height);
+        //i++;
+      }
+      time ++;
     }
+    interval = setInterval(redraw, 2);
   }
   drawManyRectangles();
 }
